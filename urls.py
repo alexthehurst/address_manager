a91b625f2af3941c99c1a52562d179091fbef45f
Alex Hurst <alexmhurst@gmail.com>
2015-08-19 03:27:49 -0400

    Stubbed out the basics of an upload page for additional addresses. Created a form class for the bulk importer. Created a view to handle the form submission. Currently it doesn't do anything with the input except for send you to the list view. Next up is tests and an actual function (class?) to break the bulk input into multiple addresses, and add them to the DB.

--- urls.py
+++ urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
-	url(r'all_addresses/', views.AllAddressesView.as_view(), name='all_addresses'),
+	url(r'^all_addresses/', views.AllAddressesView.as_view(), name='all_addresses'),
+	url(r'^bulk_import/', views.bulk_import_view, name='bulk_import'),
 ]
--- views.py
+++ views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Address
 
+from .forms import BulkImportForm
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+
 # Create your views here.
 
 class AllAddressesView(generic.ListView):
@@ -10,3 +14,14 @@ class AllAddressesView(generic.ListView):
 	def get_queryset(self):
 		"""Return all the addresses in the database."""
 		return Address.objects.all()
+
+def bulk_import_view(request):
+	if request.method=='POST':
+		#post
+		form = BulkImportForm(request.POST)
+		if form.is_valid() and len(form.cleaned_data['bulk_addresses'].split('\n')) <= 500:
+			#process
+			return HttpResponseRedirect(reverse('addman:all_addresses'))
+	else:
+		form = BulkImportForm()
+	return render(request, 'addman/bulk_import.html', {'form': form})
