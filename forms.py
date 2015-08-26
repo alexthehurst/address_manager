6d0b23dcea2c90b1e8c2d55fae23251ccd9e23f6
Alex Hurst <alexmhurst@gmail.com>
2015-08-26 15:33:13 -0700

    Added a validator for line length to the bulk_import form. Now have to figure out how to gracefully handle database errors. Maybe 500. Because this is hard to test (hardly anything can cause an address creation error yet), perhaps we don't need a unittest until the Address model is a little more complex (and easier to come up with a failing commit).

--- forms.py
+++ forms.py
@@ -9,12 +9,31 @@ def validate_bulk_import_max_records(bulk_addresses):
 			'of address data at once.',
 			code='too_many')
 
+# Keep from attempting imports if there are addresses which will fail to import.
+# I tried to use try/except to gracefully handle database import errors, but
+# exceptions are thrown after aborting an import when trying to access the DB again.
+# The solution may be to redirect to a page that doesn't hit the database, but rather a
+# simple 500 internal server error page for situations like that.
+#
+# Of course, I don't know that I could even test that, since I'm now going to gracefully catch
+# the only example I could come up with which causes a db error.
+
+def validate_bulk_import_line_length(bulk_addresses):
+    for line in bulk_addresses.split('\n'):
+        if len(line) > 1000:
+            raise ValidationError(
+                    'At least one line was too long. Please limit addresses'
+                    'to 1000 characters or fewer.'
+            )
+
 class BulkImportForm(forms.Form):
 	
 	bulk_addresses = forms.CharField(max_length=40000, 
 		widget=forms.Textarea,
 		label="Paste up to 500 addresses here, one per line.",
-		validators=[validate_bulk_import_max_records],
+		validators=[validate_bulk_import_max_records,
+           validate_bulk_import_line_length,
+        ],
 
 	)
         address_set_select = forms.ModelChoiceField(
