4a90d8476dfe08efa34b214bbba5d8305bdbdfa1
Alex Hurst <alexmhurst@gmail.com>
2015-08-26 02:05:37 -0700

    Converted my busted-ass messaging system to use the `messages` subsystem instead. More work on tests and views. Coming close to 100% coverage, but I couldn't figure out how to test 'unforeseen exceptions' in the address creation; if I know enough to trigger them for a test case, then I should catch the exception and give an intelligent error.
    At the end of everything, something broke with the test_bulk_import_with_exception_gives_error
    test case, and Google doesn't know anything about the "An error occurred in the current transaction. You can't execute queries until the end of the 'atomic' block."
    error for cases where I'm not even dealing with atomic blocks.

--- tests.py
+++ tests.py
@@ -9,13 +9,14 @@ import datetime
 class AddmanBaseTestCase(TestCase):
 	def create_address_set(self):
 		return AddressSet.objects.create(set_name='Set 1', id=1)
+
         def create_address(self,
                 user_input= "13749 Sylvan St. #1, Ellwood, CO 45371",
                 address_set_id=1):
 		return Address.objects.create(
 			user_input=user_input,
 			address_set_id=address_set_id)
-    
+
 class ModelRelationshipTests(AddmanBaseTestCase):
 	def test_can_create_address_set(self):
 		"""
@@ -28,19 +29,23 @@ class ModelRelationshipTests(AddmanBaseTestCase):
                 self.assertEqual(len(AddressSet.objects.all()), 1)
                 self.assertGreater(address_set.creation_time,
                             timezone.now() - datetime.timedelta(seconds=1))
+                self.assertIn('Set 1', address_set.__repr__())
 
                 address = self.create_address()
+                self.assertIn("13749 Sylvan St. #1, Ellwood, CO 45371", address.__repr__()) 
                 self.assertEqual(len(Address.objects.all()), 1)
                 address2 = self.create_address()
                 self.assertEqual(len(Address.objects.all()), 2)
                 self.assertGreater(address2.creation_time,
                             timezone.now() - datetime.timedelta(seconds=1))
+
 	def test_can_create_address(self):
 		"""
 		It should be possible to save an address to the DB.
 		"""
 		address_set = self.create_address_set()
 		address = self.create_address()
+
 	def test_address_creation_time_defaults_to_now(self):
 		"""
 		When an address is created with no creation_time, 
@@ -50,7 +55,6 @@ class ModelRelationshipTests(AddmanBaseTestCase):
 		address = self.create_address()
 		self.assertLess(before_time, address.creation_time)
 		self.assertLess(address.creation_time, timezone.now())
-
 
 class AllAddressViewTests(AddmanBaseTestCase):
 	def test_all_address_view_loads_when_no_data(self):
@@ -77,7 +81,9 @@ class AllAddressViewTests(AddmanBaseTestCase):
 
 
 class BulkImportViewTests(AddmanBaseTestCase):
+
 	sample_address = "54321 Main St. #42, Anytown, MA 12345"
+        
 	def test_bulk_import_view_with_get_shows_input_form(self):
 		"""
 		When given a GET request, the bulk_import view should return a
@@ -87,6 +93,7 @@ class BulkImportViewTests(AddmanBaseTestCase):
 		self.assertContains(response, 
 			'Paste up to 500 addresses here, one per line.', 
 			status_code=200)
+
 	def test_bulk_import_too_many_lines_restores_user_input_to_form(self):
 		"""
 		If a user submits a too-long form, not only should the error message show, but
@@ -98,6 +105,7 @@ class BulkImportViewTests(AddmanBaseTestCase):
                                 'address_set_select': address_set.pk}
 		)
 		self.assertTrue('bulk_addresses' in response.context['form'].data.keys())
+
 	def test_bulk_import_too_many_lines_gives_error(self):
 		"""
 		If there are more than 500 addresses, the bulk importer should 
@@ -107,6 +115,7 @@ class BulkImportViewTests(AddmanBaseTestCase):
 			data={'bulk_addresses': 501*(self.sample_address + '\n')}
 		)
 		self.assertContains(response, 'Too many records. Please provide 500 or fewer lines of address data at once')
+
 	def test_bulk_import_posting_data_works(self):
 		"""
 		POSTing some bulk address data should create DB records.
@@ -117,7 +126,8 @@ class BulkImportViewTests(AddmanBaseTestCase):
                                 'address_set_select': address_set.pk,
                         }
 		)
-		self.assertEqual(len(Address.objects.all()), 10)
+                self.assertEqual(len(Address.objects.all()), 10)
+
 	def test_bulk_import_posting_multiple_times_adds_data(self):
 		"""
 		POSTing bulk addresses when address data already exists should add, not overwrite.
@@ -134,6 +144,7 @@ class BulkImportViewTests(AddmanBaseTestCase):
                         }
 		)
 		self.assertEqual(len(Address.objects.all()), 20)
+        
 	def test_bulk_import_with_no_entry_gives_error(self):
 		"""
 		POSTing a bulk address form with no data should keep you on the same page, with 
@@ -142,3 +153,20 @@ class BulkImportViewTests(AddmanBaseTestCase):
 		response = self.client.post(reverse('addman:bulk_import'),
 			data={'bulk_addresses': ''})
 		self.assertContains(response, 'This field is required.')
+
+class MoreBulkImportViewTests(AddmanBaseTestCase):
+
+	sample_address = "54321 Main St. #42, Anytown, MA 12345"
+
+	def test_bulk_import_with_exception_gives_error(self):
+		"""
+                POSTing addresses which fail to import for any reason should keep you on the same
+                page, with an error message.
+		"""
+		address_set = self.create_address_set()
+        #import pdb; pdb.set_trace()
+		response = self.client.post(reverse('addman:bulk_import'), data={'bulk_addresses': 1001*'F', 'address_set_select': address_set.pk },
+                        follow=True,
+                )
+		self.assertContains(response, 'There was an error importing addresses.')
+		self.assertTrue('bulk_addresses' in response.context['form'].data.keys())
--- views.py
+++ views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Address
-
 from .forms import BulkImportForm
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.contrib import messages
 
 # Create your views here.
 
@@ -12,6 +12,7 @@ class AllAddressesView(generic.ListView):
 	template_name = 'addman/all_addresses.html'
 	context_object_name = 'all_addresses_list'
 	def get_queryset(self):
+
 		"""Return all the addresses in the database."""
 		return Address.objects.all()
 
@@ -19,18 +20,30 @@ def bulk_import_view(request):
 	if request.method=='POST':
 		form = BulkImportForm(request.POST)
 		if not form.is_valid():
-			return render(request, 'addman/bulk_import.html', context={'form': form, })
+			return render(request,
+                          'addman/bulk_import.html',
+                           context={'form': form, })
 		else:
 		 	user_lines = form.cleaned_data['bulk_addresses'].strip().split('\n')
 			for line in user_lines:
 				try:
-					Address.objects.create(user_input=line, address_set_id=form.cleaned_data['address_set_select'].pk)
+					Address.objects.create(user_input=line,
+                        address_set_id = form.cleaned_data['address_set_select'].pk
+                    )
 				except:
-					notice = "There was an error importing addresses. The import was stopped at '%s'." % line
-					return render(request, 'addman/bulk_import.html', context={'form': form, })
-			notice = "Thanks for the submission. Those addresses have been imported."
-			return HttpResponseRedirect(reverse('addman:all_addresses'), )
+					messages.error(request, "There was an error importing addresses."
+                        "The import was stopped at '%s'." % line)
+					return render(request,
+                            'addman/bulk_import.html',
+                            context={'form': form, },
+                    )
+			messages.success(request, "Thanks for the submission."
+                    "Those addresses have been imported.")
+			return HttpResponseRedirect(reverse('addman:all_addresses'))
 	
 	else: # GET
 		form = BulkImportForm()
-		return render(request, 'addman/bulk_import.html', context={'form': form,} )
+		return render(request,
+                'addman/bulk_import.html',
+                context={'form': form,}
+        )
