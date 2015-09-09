747549437fd34b51368b319487bd0d7522a7b3a0
Alex Hurst <alexmhurst@gmail.com>
2015-09-08 22:14:57 -0700

    Add address detail view to urls
    Add address detail view, add all_addresses as the default ("^/$"),
    and re-wrapped a couple of lines in urls.py (because I got my
    syntax highlighting to work properly).

--- urls.py
+++ urls.py
@@ -3,6 +3,17 @@ from . import views
 from django.views.generic import TemplateView
 
 urlpatterns = [
-	url(r'^all_addresses/', views.AllAddressesView.as_view(), name='all_addresses'),
+	url(r'^detail/(?P<pk>[0-9]+)',
+        views.AddressDetailView.as_view(),
+        name='detail'
+    ),
+	url(r'^$',
+        views.AllAddressesView.as_view(),
+        name='all_addresses'
+    ),
+	url(r'^all_addresses/',
+        views.AllAddressesView.as_view(),
+        name='all_addresses'
+    ),
 	url(r'^bulk_import/', views.bulk_import_view, name='bulk_import'),
 ]
