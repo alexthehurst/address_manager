d706dbb0a41a41a971e893a7c1966b0ed3174863
Alex Hurst <alexmhurst@gmail.com>
2016-03-04 00:17:07 -0800

    Update set_user_input for the new model

--- models.py
+++ models.py
@@ -74,5 +74,7 @@ class Address(models.Model):
         self.street = ''
         self.city = ''
         self.state = ''
-        self.zip = ''
+        self.zip5 = ''
+        self.zip4 = ''
+
         self.save()
